c0dd3fea637917092e88b6f88c282331bd336976
Nikki <piknik@me.com>
2020-12-10 15:58:48 -0700

    added some basic structure for createNewBook

--- src/main.js
+++ src/main.js
@@ -37,6 +37,35 @@ function updateCover() {
 }
 
 
+// create new book button
+
+var userCover = document.querySelector('input.user-cover');
+var userTitle = document.querySelector('input.user-title');
+var userDescriptor1 = document.querySelector('input.user-desc1');
+var userDescriptor2 = document.querySelector('input.user-desc2');
+
+var createNewBookButton = document.querySelector('button.create-new-book-button');
+createNewBookButton.addEventListener('submit', createNewBook);
+
+function createNewBook() {
+  // push each piece of data into its respective array in data.js
+  covers.push(userCover.value)
+  titles.push(userTitle.value)
+  descriptors.push(userDescriptor1.value)
+  descriptors.push(userDescriptor2.value)
+
+  // build new cover from class and unshift that to the savedCovers, so we can access it when we go
+  // back to the home page
+  var userCreatedCover = buildNewCover(userCover.value, userTitle.value, userDescriptor1.value, userDescriptor2.value);
+  savedCovers.unshift(userCreatedCover)
+
+  // change back to home by adding hidden to form-view and removing it from home
+
+  // when at home, should load THIS new cover
+  // use unshift to add to front, then we can use index 0 to access the first one.
+}
+
+
 // We've provided one function to get you started
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
